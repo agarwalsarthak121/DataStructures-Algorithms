f7b9c4ff7cb4021d2813630748c4ec2e9dc73231
Sarthak agarwal <agarwalsarthak121@gmail.com>
2019-08-06 15:02:39 +0530

    Create sqrOfSortArr.java

---
+++ Arrays/sqrOfSortArr.java
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/squares-of-a-sorted-array/submissions/
+*/
+class Solution {
+    public int[] sortedSquares(int[] A) {
+        if (A == null) return null;
+        
+        int [] ans = new int[A.length];
+     
+        int start = 0, end = A.length - 1;
+        int i = end; // insert position.
+        while (start <= end) { // <  or <=  ?   be careful about ==
+            int pow1 = A[start] * A[start];
+            int pow2 = A[end] * A[end];
+            if (pow1 > pow2) {
+                ans[i--] = pow1;
+                start++;
+            } else {
+                ans[i--] = pow2;
+                end--;
+            }
+        }
+        return ans;
+    }
+}
